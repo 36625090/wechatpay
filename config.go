4a99b85738cd196984b6788d956752e507226b93
liping <36625090@qq.com>
2023-03-07 14:56:32 +0800

    fix logger

--- config.go
+++ config.go
@@ -1,12 +1,12 @@
 package wechatpay
 
 type Config struct {
-	AppID                      string `json:"app_id"`
-	MchID                      string `json:"mch_id"`
-	MchAPIv3Key                string `json:"mch_api_v3_key"`
-	MchCertificateSerialNumber string `json:"mch_certificate_serial_number"`
-	PaymentNotifyURL           string `json:"payment_notify_url"`
-	RefundNotifyURL            string `json:"refund_notify_url"`
-	PrivateKey                 string `json:"private_key"`
-	PublicKey                  string `json:"public_key"`
+	AppID                      string `json:"app_id" hcl:"app_id"`
+	MchID                      string `json:"mch_id" hcl:"mch_id"`
+	MchAPIv3Key                string `json:"mch_api_v3_key" hcl:"mch_api_v3_key"`
+	MchCertificateSerialNumber string `json:"mch_certificate_serial_number" hcl:"mch_certificate_serial_number"`
+	PaymentNotifyURL           string `json:"payment_notify_url" hcl:"payment_notify_url"`
+	RefundNotifyURL            string `json:"refund_notify_url" hcl:"refund_notify_url"`
+	PrivateKey                 string `json:"private_key" hcl:"private_key"`
+	PublicKey                  string `json:"public_key" hcl:"public_key"`
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/36625090/wechatpay
 
 go 1.17
 
-require github.com/wechatpay-apiv3/wechatpay-go v0.2.14
+require github.com/wechatpay-apiv3/wechatpay-go v0.2.16
